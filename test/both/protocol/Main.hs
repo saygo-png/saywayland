cae61ad41d7b69c3fd337d68592dddc14bb210bc
saygo-png <saygo.mail@proton.me>
2026-05-15 15:04:27 +0200

    fix template haskell test
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- test/both/protocol/Main.hs
+++ test/both/protocol/Main.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Main (main) where
 
 import Language.Haskell.TH
 import Protocol
-import Saywayland.Interfaces
 import Prelude
 
 main :: IO ()
 main = do
-  runQ (loadProtocols (ConT ''Wayland) True "protocols") >>= writeFile "output.th.hs" . pprint
+  runQ (loadProtocols True "protocols") >>= writeFile "output.th.hs" . pprint
