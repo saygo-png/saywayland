af04bb5e18150f23b57a83234aaed30ab86e5781
FokoHetman <foko@hetman.at>
2026-06-07 02:51:17 +0200

    partial XDG Shell implementation

--- lib/Saywayland.hs
+++ lib/Saywayland.hs
@@ -2,10 +2,11 @@
 Module      : Saywayland
 Description : Module containing all of Saywayland.
 -}
-module Saywayland (module Protocol, module Saywayland.Protocols.Wayland, module Saywayland.Protocols.WlrLayerShell, module Saywayland.Types, module Saywayland.WaylandSocket, module SaywaylandTH) where
+module Saywayland (module Protocol, module Saywayland.Protocols.Wayland, module Saywayland.Protocols.XdgShell, module Saywayland.Protocols.WlrLayerShell, module Saywayland.Types, module Saywayland.WaylandSocket, module SaywaylandTH) where
 
 import Protocol
 import Saywayland.Protocols.Wayland
+import Saywayland.Protocols.XdgShell
 import Saywayland.Protocols.WlrLayerShell
 import Saywayland.Types
 import Saywayland.WaylandSocket
